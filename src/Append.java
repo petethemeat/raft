fe27c469bd2facc6d3498f27096c1faaef0651d0
Paul Cozzi <Paul Cozzi@Paul_Cozzi_PC>
2017-04-20 15:06:11 -0500

    Append message finished and ready for debug Added toString() to Server.LogEntry class

--- src/Append.java
+++ src/Append.java
@@ -1,3 +1,8 @@
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Append { //not a runnable anymore, make your own thread to run in parallel
 	private int term; //leader's term
@@ -6,13 +11,14 @@ public class Append { //not a runnable anymore, make your own thread to run in p
 	private int prevLogTerm; //term of prevLogIndex entry
 	private Server.LogEntry[] entries; //empty for heartbeat, may send more tan one for efficiency
 	private int leaderCommit; //leader's commitIndex
-	private String recipient;
+	private String recipientIP;
+	private int recipientPort;
 	
-	private int returnTerm; //returned term, for the leader to updatte itself
+	private int returnTerm; //returned term, for the leader to update itself
 	private Boolean success; //true iff follower contained entry matching prevLogIndex and prevLogTerm
 							//null if not finished
 	public Append(int term, int leaderID, int prevLogIndex, 
-				int prevLogTerm, int leaderCommit, Server.LogEntry[] entries, String recipient)
+				int prevLogTerm, int leaderCommit, Server.LogEntry[] entries, String recipientIP, int recipientPort)
 	{
 		this.term = term;
 		this.leaderID = leaderID;
@@ -20,11 +26,35 @@ public class Append { //not a runnable anymore, make your own thread to run in p
 		this.prevLogTerm = prevLogTerm;
 		this.leaderCommit = leaderCommit;
 		this.entries = entries; //WARNING, does not copy array
-		this.recipient = recipient;
+		this.recipientIP = recipientIP;
+		this.recipientPort = recipientPort;
 	}
 	
-	public Boolean run() { 
+	//Attempts to send once, with timeout of one second
+	public Boolean send() { 
 		//TODO: Connect to recipient, receive and update response
+		Socket sock = new Socket();
+		String message = "append " + term + " " + leaderID + " " + prevLogIndex + " " + prevLogTerm + " " + leaderCommit;
+		for(int i = 0; i < entries.length; ++i){
+			message = message + " " + entries[i].toString();
+		}
+		try{
+			sock.setSoTimeout(1000);
+			sock.connect(new InetSocketAddress(recipientIP, recipientPort), 1000);
+			PrintStream pout = new PrintStream(sock.getOutputStream());
+			pout.println(message);
+			Scanner sc = new Scanner(sock.getInputStream());
+			
+			//expects single line response, in space-delimited form: success returnTerm
+			success = sc.nextBoolean();
+			returnTerm = sc.nextInt();
+			sock.close();
+			pout.close();
+			sc.close();
+			return success;
+		}catch(Exception e){
+			return null;
+		}
 
 	}
 	
--- src/Server.java
+++ src/Server.java
@@ -21,6 +21,7 @@ public class Server
 	//We need to send over TCP anyway
 	public class LogEntry
 	{
+		//TODO: make constructor
 		/* Possible entries:
 		 * purchase <user-name> <product-name> <quantity>
 		 * cancel <order-id>
@@ -30,9 +31,24 @@ public class Server
 		int cmd; //0 = purchase, 1 = cancel, 2 = search, 3 = list, -1 = null
 		String clientIP; //formatted "IpAddress:Port"
 		int serialNum; //unique serial number, provided by the client
-		String arg1; //a username, if needed
-		String arg2; //product name, if needed
-		int arg; //quantity or order ID, if needed
+		String arg1; //a username if needed, null otherwise
+		String arg2; //product name if needed, null otherwise
+		int arg; //quantity or order ID if needed, -1 otherwise
+		
+		@Override
+		public String toString(){
+			String ret = cmd + " ";
+			ret = ret + clientIP + " ";
+			ret = ret + serialNum + " ";
+			if(arg1 != null){
+				ret = ret + arg1 + " ";
+			}
+			if(arg2 != null){
+				ret = ret + arg2 + " ";
+			}
+			ret = ret + arg + " ";
+			return ret;
+		}
 	}
 	
 	
