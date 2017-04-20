1e668f611e4e93f2884aea6fc8ccb3562dc981c8
Paul Cozzi <Paul Cozzi@Paul_Cozzi_PC>
2017-04-19 20:03:24 -0500

    began Append, added LogEntry class to Server

--- src/Append.java
+++ src/Append.java
@@ -1,6 +1,10 @@
 
 public class Append implements Runnable {
 
+	public Append(int term, int leaderID, int prevLogIndex, 
+				int prevLogTerm, int leaderCommit, Server.LogEntry[] entries){
+		
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
--- src/Server.java
+++ src/Server.java
@@ -17,6 +17,24 @@ public class Server
 		this.role = role;
 	}
 	
+	//An entry for the log. I didn't form separate classes for each command because
+	//We need to send over TCP anyway
+	public class LogEntry
+	{
+		/* Possible entries:
+		 * purchase <user-name> <product-name> <quantity>
+		 * cancel <order-id>
+		 * search <user-name>
+		 * list
+		 */
+		int cmd; //0 = purchase, 1 = cancel, 2 = search, 3 = list, -1 = null
+		String clientIP; //formatted "IpAddress:Port"
+		int serialNum; //unique serial number, provided by the client
+		String arg1; //a username, if needed
+		String arg2; //product name, if needed
+		int arg; //quantity or order ID, if needed
+	}
+	
 	
 
 }
